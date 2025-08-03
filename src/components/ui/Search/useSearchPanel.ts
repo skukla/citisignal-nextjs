a8d005f6a87068f2ac92e5984c4f6089bb918337
Steve Kukla <kukla@adobe.com>
2025-08-03 18:12:24 -0400

    refactor(ui): Remove old component files
    Remove old component files that have been moved to their new
    categorized locations. This completes the component reorganization.

--- src/components/ui/Search/useSearchPanel.ts
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { usePanel } from '@/hooks/usePanel';
-import type { UsePanelReturn } from '@/hooks/usePanel';
-
-export function useSearchPanel(): UsePanelReturn {
-  return usePanel();
-} 
